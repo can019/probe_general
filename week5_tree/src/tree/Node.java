6b0a9a054521571177643b6a3bce27122f075c3f
can019 <jys01012@gmail.com>
2020-04-14 01:50:52 +0900

    init Project :: Tree_week5, Node class added

---
+++ week5_tree/src/tree/Node.java
@@ -0,0 +1,36 @@
+package tree;
+
+public class Node<T> {
+    private T data;
+    private Node<T> next;
+    private Node<T> pre;
+
+    public Node(){
+        this.data = null;
+        this.next = null;
+        this.pre = null;
+    }
+    public Node(T data) {
+        this.data = data;
+        this.next= null;
+        this.pre = null;
+    }
+    public Node<T> getNext() {
+        return this.next;
+    }
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+    public void setData(T data) {
+        this.data = data;
+    }
+    public T getData() {
+        return this.data;
+    }
+    public Node<T> getPre() {
+        return this.pre;
+    }
+    public void setPre(Node<T> pre) {
+        this.pre = pre;
+    }
+}
