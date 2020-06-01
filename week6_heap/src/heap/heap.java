6a50b291e6b266d822578eba2109e53075427af6
can019 <jys01012@gmail.com>
2020-06-02 00:11:38 +0900

    intiailize

--- week6_heap/src/heap/heap.java
+++ week6_heap/src/heap/heap.java
@@ -1,4 +1,26 @@
 package heap;
 
 public class heap {
+    private final int DEFAULT_HEAP_SIZE = 10;
+    private int [] heap;
+    private int size;
+
+    public heap(){
+        heap = new int[DEFAULT_HEAP_SIZE];
+        this.setSize(0);
+    }
+    public heap(int size){
+        this.heap = new int[size];
+        this.setSize(0);
+    }
+
+    public int size(){
+        return this.size;
+    }
+    public void setSize(int size){
+        this.size = size;
+    }
+
+
+
 }
